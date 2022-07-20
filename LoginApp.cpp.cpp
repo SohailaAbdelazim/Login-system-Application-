3f9f33a320e7acdfc0cc44088d326cd62caac02c
Sohaila Abdelazim <100935067+SohailaAbdelazim@users.noreply.github.com>
2022-07-20 19:13:21 +0300

    Update LoginApp.cpp.cpp

--- LoginApp.cpp.cpp
+++ LoginApp.cpp.cpp
@@ -1,10 +1,8 @@
-// FCAI – Programming 1 – 2022 - Assignment 4.
-// Program Name: CS112-203-2nd - S9,10 -20210090-20210492-20211109-A4- LoginApp.cpp
+
 // Last Modification Date: 13/05/2022
-// Author1 and ID and Group: Basmala Mohamed Sayed Gad  ID: 20210090  Group: s10 - Functions : 1 - 5
-// Author2 and ID and Group: Sohaila Abdelazim Khalifa  ID: 20210492  Group: s10 - Functions : 6 - 10
-// Author3 and ID and Group: Hadeer Abdelnasser         ID: 20211109  Group: s10 - Functions : 11 - 15
-// Teaching Assistant: Afaf Abdelmonem
+// Author1  Sohaila Abdelazim Khalifa 
+// Author2  Basmala Mohamed Sayed Gad   
+// Author3  Hadeer Abdelnasser        
 /* Purpose: The purpose of this program is to allow a new user to register in the system or to login to the system as
    he already registered earlier , or he can change his password .*/
 
